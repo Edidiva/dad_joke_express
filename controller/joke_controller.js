9fa31e34dec2e67fe3e77fec57cf57f0ecc5377d
Edidiva <edikanakpan5@gmail.com>
2023-05-09 20:35:15 +0100

    update

--- controller/joke_controller.js
+++ controller/joke_controller.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const API_URL = 'https://dad-jokes.p.rapidapi.com/random/joke';
 const API_HEADERS = {
-  'x-rapidapi-key': process.env.API_KEY,
+  'x-rapidapi-key':process.env.API_KEY,
   'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
   'useQueryString': true
 };
@@ -12,13 +14,15 @@ const getRandomDadJoke = async (req, res, next) => {
     const request = await axios.get(API_URL, { headers: API_HEADERS });
     const joke =`setup: ${request.data.body[0].setup}, \npunchline: ${request.data.body[0].punchline}`;
     res.json({ joke });
+    
   } catch (error) {
+    console.log(error)
     next( new Error ('failed to get joke'));
   }
 };
 
 module.exports = {
-  getRandomDadJoke,
+  getRandomDadJoke
 };
 
 
